1d1e3efa488947f91d6bb2674c68619f7ac2c87e
Momshad Dinury <md.dinury@gmail.com>
2021-03-12 13:05:23 +0600

    Update nwes_model.dart

---
+++ lib/model/nwes_model.dart
@@ -0,0 +1,101 @@
+import 'dart:convert';
+
+class NewsModel {
+  NewsModel({
+    this.status,
+    this.totalResults,
+    this.articles,
+  });
+
+  String status;
+  int totalResults;
+  List<Article> articles;
+
+  factory NewsModel.fromRawJson(String str) => NewsModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory NewsModel.fromJson(Map<String, dynamic> json) => NewsModel(
+    status: json["status"],
+    totalResults: json["totalResults"],
+    articles: List<Article>.from(json["articles"].map((x) => Article.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "status": status,
+    "totalResults": totalResults,
+    "articles": List<dynamic>.from(articles.map((x) => x.toJson())),
+  };
+}
+
+class Article {
+  Article({
+    this.source,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
+
+  Source source;
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  DateTime publishedAt;
+  String content;
+
+  factory Article.fromRawJson(String str) => Article.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Article.fromJson(Map<String, dynamic> json) => Article(
+    source: Source.fromJson(json["source"]),
+    author: json["author"],
+    title: json["title"],
+    description: json["description"],
+    url: json["url"],
+    urlToImage: json["urlToImage"],
+    publishedAt: DateTime.parse(json["publishedAt"]),
+    content: json["content"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "source": source.toJson(),
+    "author": author,
+    "title": title,
+    "description": description,
+    "url": url,
+    "urlToImage": urlToImage,
+    "publishedAt": publishedAt.toIso8601String(),
+    "content": content,
+  };
+}
+
+class Source {
+  Source({
+    this.id,
+    this.name,
+  });
+
+  String id;
+  String name;
+
+  factory Source.fromRawJson(String str) => Source.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Source.fromJson(Map<String, dynamic> json) => Source(
+    id: json["id"] == null ? null : json["id"],
+    name: json["name"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id == null ? null : id,
+    "name": name,
+  };
+}
